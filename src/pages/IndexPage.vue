2d9cef650748a46162527f30b10ccb378257fcc7
Nadilson <nadilson@protonmail.com>
2022-11-11 00:23:12 -0300

    Adicionado seletor de arquivos (file chooser)

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,36 @@
 <template>
+  <div id="waveform"></div>
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import WaveSurfer from "wavesurfer.js";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  data: () => ({
+    wavesurfer: null,
+  }),
+  async mounted() {
+    if (!this.wavesurfer) this.createWaveSurfer();
+  },
+  methods: {
+    createWaveSurfer() {
+      this.wavesurfer = WaveSurfer.create({
+        container: "#waveform",
+        barWidth: 3,
+      });
+      this.wavesurfer.load(
+        "https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3"
+      );
+    },
+  },
+});
 </script>
